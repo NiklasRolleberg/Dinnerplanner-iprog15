afd99e8c3208a6afe8163cf56bcbe24840c7bab5
NiklasRolleberg <niklasr93@gmail.com>
2015-02-08 17:05:43 +0100

    lite nya saker

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -1,14 +1,19 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,17 @@ public class MainActivity extends Activity {
 
         // Set the view for the main activity screen
         // it must come before any call to findViewById method
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.page01_layout);
+        start = (Button)findViewById(R.id.p1_button);
+        start.setOnClickListener(this);
         // Creating the view class instance
-        ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
+        //ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
     }
 
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(this, Page2Activity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/Page2Activity.java
@@ -0,0 +1,14 @@
+package se.kth.csc.iprog.dinnerplanner.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Page2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page02_layout);
+    }
+}
